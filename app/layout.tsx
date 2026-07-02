fabaee8c75267a934df2b64f26d2398dec14f7ad
fanyumeng16-hub <fanyumeng16@gmail.com>
2026-07-02 16:00:41 -0400

    6.0v

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Albert_Sans } from "next/font/google";
 import "@fontsource/bitcount-grid-single/400.css";
 import "@fontsource/bitcount-grid-single/700.css";
+import { MicrosoftClarity } from "@/components/MicrosoftClarity";
 import "./globals.css";
 
 const albertSans = Albert_Sans({
@@ -28,7 +29,10 @@ export default function RootLayout({
       className={`${albertSans.variable} h-full antialiased`}
       data-scroll-behavior="smooth"
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <MicrosoftClarity />
+      </body>
     </html>
   );
 }
